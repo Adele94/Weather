b5f4c5015c83ab0860a8dc6a96d1232700709be1
 Adele Shavalieva <adel.shavalieva@yandex.ru>
2019-10-31 21:12:25 +0300

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by  Adele Shavalieva on 31/10/2019.
+//  Copyright © 2019  Adele Shavalieva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
